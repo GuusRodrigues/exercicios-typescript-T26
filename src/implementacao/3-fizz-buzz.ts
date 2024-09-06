853553ca6a9fd379c0bad891342d1369bde8b73c
Gustavo Rodrigues <gustarodrigues862@gmail.com>
2024-09-06 11:38:07 -0300

    Exercício 3

--- src/implementacao/3-fizz-buzz.ts
+++ src/implementacao/3-fizz-buzz.ts
@@ -1,4 +1,16 @@
-function fizzBuzz(numero: number) {
-    return null;
+function fizzBuzz(numero: number): string | number {
+
+    if (numero % 5 === 0 && numero % 3 === 0) { 
+        return "FizzBuzz";
+    } 
+    else if (numero % 5 === 0) { 
+        return "Buzz";
+    } 
+    else if (numero % 3 === 0) { 
+        return "Fizz";
+    } 
+    else { 
+        return numero.toString(); //transforma o número em string
+    }
 }
 module.exports = fizzBuzz;

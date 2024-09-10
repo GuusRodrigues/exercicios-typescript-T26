3d5f32c51bfed9bcadeba42f605153a54527f45b
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-09-10 15:34:40 -0300

    Andressa Teste

--- test/1-hello-world.test.js
+++ test/1-hello-world.test.js
@@ -1,5 +1,5 @@
 const hello = require('../src/implementacao/1-hello-world');
-describe('Hello world', () => { 
+describe('Hello world!!', () => { 
     test('Diz Hello World', () => {
         expect(hello()).toBe('Hello world');
     });

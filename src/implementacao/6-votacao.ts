42ea13392273f5f424bb689d2db4bb163b1d0ec5
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-09-11 16:27:24 -0300

    6-votação melhorias

--- src/implementacao/6-votacao.ts
+++ src/implementacao/6-votacao.ts
@@ -1,17 +1,28 @@
 function apuraVotacao(votos: number[]): string {
 
-    let votos1 = 0, votos2 = 0, votos3 = 0;
+    let votos1 = 0;
+    let votos2 = 0;
+    let votos3 = 0;
 
     for (let voto of votos) {
-        if (voto === 1) votos1++;
-        else if (voto === 2) votos2++;
-        else if (voto === 3) votos3++;
+        if (voto === 1) {
+            votos1++;
+        } else if (voto === 2) {
+            votos2++;
+        } else if (voto === 3) {
+            votos3++;
+        }
     }
 
-    if (votos1 > votos2 && votos1 > votos3) return "Vencedor: 1";
-    if (votos2 > votos1 && votos2 > votos3) return "Vencedor: 2";
-    if (votos3 > votos1 && votos3 > votos2) return "Vencedor: 3";
-
-    return "Empate";
+    if (votos1 > votos2 && votos1 > votos3) {
+        return "Vencedor: 1";
+    } else if (votos2 > votos1 && votos2 > votos3) {
+        return "Vencedor: 2";
+    } else if (votos3 > votos1 && votos3 > votos2) {
+        return "Vencedor: 3";
+    } else {
+        return "Empate";
+    }
 }
+
 module.exports = apuraVotacao;

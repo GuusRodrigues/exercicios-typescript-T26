00990e0ea80a7927bfbc8dfa92682b491c54f818
Camile Stefany <camilestefanypereira12@gmail.com>
2024-09-11 14:00:14 -0300

    ContadordeVogais - Feito

--- src/implementacao/4-contador-de-ocorrencia.ts
+++ src/implementacao/4-contador-de-ocorrencia.ts
@@ -1,4 +1,18 @@
-function contadorDeOcorrencia(texto: string, letra: string) {
-    return null;
+function contaOcorrencias(texto: string, busca: string): number {
+    // Converte as entradas para minúsculas para uma busca case-insensitive
+
+    // Inicializa o contador
+    let contador = 0;
+    let posicao = texto.indexOf(busca);
+
+    // Conta as ocorrências enquanto a busca for encontrada no texto
+    while (posicao !== -1) {
+        contador = contador + 1.
+        // Move para a próxima posição no texto após a última ocorrência encontrada
+        posicao = texto.indexOf(busca, posicao + 1);
+    }
+
+    return contador;
 }
-module.exports = contadorDeOcorrencia;
+
+module.exports = contaOcorrencias;

ff847ae953b8a56dca7d8d9595c2aaddeedde950
Andressa-Limas <agfl@discente.ifpe.edu.br>
2024-09-11 16:06:27 -0300

    7-numeros-armstrong

--- src/implementacao/7-numeros-armstrong.ts
+++ src/implementacao/7-numeros-armstrong.ts
@@ -1,4 +1,15 @@
-function numerosArmstrong(numero: number) {
-    return null;
+function numerosArmstrong(numero) {
+    let valor = 0;
+
+    for (let n of String(numero)) {
+        valor += Number(n) ** (String(numero)).length;
+    }
+    
+    if (numero === valor) {
+        return "Este eh um numero de Armstrong!";
+    } else {
+        return "Este nao eh um numero de Armstrong!";
+    }
 }
+
 module.exports = numerosArmstrong;

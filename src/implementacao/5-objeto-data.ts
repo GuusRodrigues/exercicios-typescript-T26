464899cfcc8c20043da8522f27334e302a77e942
Camile Stefany <camilestefanypereira12@gmail.com>
2024-09-11 14:24:34 -0300

    criaObjeto - feito

--- src/implementacao/5-objeto-data.ts
+++ src/implementacao/5-objeto-data.ts
@@ -1,4 +1,9 @@
 function criaObjeto(data: string) {
-    return null;
+    // Divide a string da data no formato "dd/mm/aaaa"
+    const [dd, mm, aaaa] = data.split('/');
+
+    // Retorna um objeto com as partes da data
+    return { dia: dd, mes: mm, ano: aaaa };
 }
+
 module.exports = criaObjeto;
